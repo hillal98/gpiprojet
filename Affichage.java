4d5f4d989837c17e52318bdd0544c69d3a3f1ba1
walidme <78801359+walidme@users.noreply.github.com>
2021-03-23 20:21:05 +0100

    Update Affichage.java

--- Affichage.java
+++ Affichage.java
@@ -7,14 +7,14 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-import com.mathmaurer.objets.Bloc;
-import com.mathmaurer.objets.TuyauRouge;
-import com.mathmaurer.personnages.Mario;
+import groupesourire.objets.Bloc;
+import groupesourire.objets.Deplacement;
+import groupesourire.objets.Bouton;
 
 
 //***** La classe Affichage est le classe la plus importante de l'application. *****//
 //***** Elle est accessible par toutes les autres classes, contient le "moteur" de l'application *****//
-//***** Elle gère la partie graphique. *****//
+//***** Elle gÃ¨re la partie graphique. *****//
 @SuppressWarnings("serial")
 public class Affichage extends JPanel {
 	
@@ -31,7 +31,7 @@ public class Affichage extends JPanel {
 	
 	private int xFond1;
 	private int xFond2;
-	private int dx;   // déplacement du fond d'écran.
+	private int dx;   // dÃ©placement du fond d'Ã©cran.
 	private int xPos; // position absolue dans l'appli
 	
 	public Click click;
@@ -86,15 +86,15 @@ public class Affichage extends JPanel {
 
 
 	//**** METHODES ****//
-	public void deplacementFond(){ // Déplacement des images "fixes" de l'écran 
+	public void deplacementFond(){ // DÃ©placement des images "fixes" de l'Ã©cran 
 		
 		if(this.xPos >= 0){
-			// Mise à jour des positions des éléments de l'appli
+			// Mise Ã  jour des positions des Ã©lÃ©ments de l'appli
 			this.xPos = this.xPos + this.dx;		
 		    this.xFond1 = this.xFond1 - this.dx;
 		    this.xFond2 = this.xFond2 - this.dx;
 		}
-		// Permanence du fond d'écran
+		// Permanence du fond d'Ã©cran
 		if(this.xFond1 == -800){this.xFond1 = 800;}
 		else if(this.xFond2 == -800){this.xFond2 = 800;}
 		else if(this.xFond1 == 800){this.xFond1 = -800;}
@@ -102,12 +102,12 @@ public class Affichage extends JPanel {
 	}
 
 
-	public void paintComponent(Graphics g) { // Dessin de toutes les images visibles à l'écran (appel toutes les 3 ms environ)
+	public void paintComponent(Graphics g) { // Dessin de toutes les images visibles Ã  l'Ã©cran (appel toutes les 3 ms environ)
 		
 		super.paintComponent(g);
 		Graphics g2 = (Graphics2D)g;
 
-		//Détection 
+		//DÃ©tection 
 		if(this.click.contact() == true){
 			this.click.setMarche(false);
 			this.dx = 0;
@@ -119,8 +119,8 @@ public class Affichage extends JPanel {
 		g2.drawImage(this.imgFond1, this.xFond1, 0, null); // Fond1
 		g2.drawImage(this.imgFond2, this.xFond2, 0, null); // Fond 2
  		g2.drawImage(this.click.marche("click", 25), 300, 245, null); //*** code provisoire
- 		g2.drawImage(this.imgBouton1, 10 - this.xPos, 95, null); // Bouton du début
- 		g2.drawImage(this.imgDepart, 220 - this.xPos, 234, null); // Panneau départ
+ 		g2.drawImage(this.imgBouton1, 10 - this.xPos, 95, null); // Bouton du dÃ©but
+ 		g2.drawImage(this.imgDepart, 220 - this.xPos, 234, null); // Panneau dÃ©part
  		g2.drawImage(this.Bouton1.getImgBouton1(), this.Bouton1.getX(), this.Bouton1.getY(), null);
  		g2.drawImage(this.bloc1.getImgBloc(), this.bloc1.getX() - this.xPos, this.bloc1.getY(), null);
 	}
